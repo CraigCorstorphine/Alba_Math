b75995a8c72fedd71f0cce33f5de82de05afa614
craigcorstorphine <emgpersonaltraining@gmail.com>
2019-11-21 17:04:16 +0000

    Initial commit

---
+++ app/src/main/java/com/craigcorstorphine/myapplication/MainActivity.java
@@ -0,0 +1,171 @@
+package com.craigcorstorphine.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import static java.lang.Integer.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar mProgressBar;
+    private TextView mLoadingText;
+
+    private int mProgressStatus = 0;
+
+    private Handler mHandler = new Handler();
+
+    int value3;
+    int answer;
+    EditText answer_field;
+    ProgressBar progressBar;
+    int score;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setNumber();
+        progressBar = (ProgressBar) findViewById(R.id.answerProgress);
+        progressBar.setMax(10);
+        progressBar.setProgress(0);
+
+
+
+
+
+
+
+        Button myButton = findViewById(R.id.answer_button);
+
+
+
+
+
+
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(!answer_field.getText().toString().equals("")){
+
+                        answer = valueOf(answer_field.getText().toString());
+                        //value3 = value1 * value2;
+
+
+                        if(value3 == answer)
+                        {
+                            setNumber();
+
+                            showToast("Correct");
+                            TextView Scores = (TextView) findViewById(R.id.scoreText);
+                            score ++;
+                            progressBar.setProgress(score);
+                            endGame();
+
+
+
+                        }
+                        else {
+                            showToast("wrong");
+                            setNumber();
+                        }
+
+                    }
+
+                }
+
+             });
+
+    }
+
+    public void setNumber(){
+        final ImageView leftNumber = findViewById(R.id.left_number);
+
+        final ImageView rightNumber = findViewById(R.id.right_number);
+        final int[] numberArray = new int[]{
+                R.drawable.number_0,
+                R.drawable.number1,
+                R.drawable.number2,
+                R.drawable.number3,
+                R.drawable.number4,
+                R.drawable.number5,
+                R.drawable.number6,
+                R.drawable.number7,
+                R.drawable.number8,
+                R.drawable.number9,
+        };
+        // Create a random number generator
+        final Random randomNumberGenerator = new Random();
+
+
+        final int value1 = randomNumberGenerator.nextInt(10);
+
+
+
+
+        leftNumber.setImageResource(numberArray[value1]);
+
+        // Create a new random number
+        final int value2 = randomNumberGenerator.nextInt(10);
+
+        // Set the right dice image using an image from the diceArray.
+        rightNumber.setImageResource(numberArray[value2]);
+        answer_field = findViewById(R.id.answer_field);
+
+
+
+        value3 = value1 * value2;
+        
+    }
+
+
+
+
+
+
+
+    public void showToast(String text){
+        Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
+    }
+    private void endGame() {
+        // This takes the modulus. Not a division.
+
+
+        // Present an alert dialog if we reach the end.
+        if(score == 10) {
+            // Don't use:
+            // AlertDialog.Builder alert = new AlertDialog.Builder(getApplicationContext());
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+
+            alert.setTitle("Game Over");
+            alert.setCancelable(false);
+            alert.setMessage("Well done!!! you got  " + score + " correct!");
+            alert.setPositiveButton("Close Application", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            });
+            alert.show();
+        }
+
+
+}}
